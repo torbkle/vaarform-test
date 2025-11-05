28041be27e43a735ae800e83cf66b61798982ade
torbkle <torkle05553@stud.noroff.no>
2025-11-05 18:30:09 +0100

    Update auth.py

--- components/auth.py
+++ components/auth.py
@@ -5,7 +5,7 @@ from datetime import datetime
 # === Supabase-klient ===
 SUPABASE_URL = st.secrets["SUPABASE_URL"]
 SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
-supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 # === Innlogging ===
 def login(email, password):
